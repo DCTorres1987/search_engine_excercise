41eca3c563845c12601e30509768765b050c6767
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-19 22:56:46 -0400

    work importing the mock data and mapping to the DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import JSONDATA from './data/MOCK_DATA.json'
 
 function App() {
   return (
     <div className="App">
-      Hello is it me you are looking for..
+      <input type="text" placeholder="Search..." />
+      {JSONDATA.map((val,key) => {
+        return (
+          <div className="animal" key={key}>
+            <p>{val.Name} </p>
+          </div>
+        )
+      })}
     </div>
   );
 }
