86b7650fe1b7b620b7700a53fac301872b0a88fa
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-26 00:02:27 -0400

    work on filtering the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
 import JSONDATA from './data/MOCK_DATA.json'
+import {useState} from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <div className="App">
-      <input type="text" placeholder="Search..." />
-      {JSONDATA.map((val,key) => {
+      <input type="text" 
+             placeholder="Search..." 
+             onChange={event => {
+                setSearchTerm(event.target.value)
+          }}
+          />
+
+      {JSONDATA.filter((val) => {
+        if (searchTerm === "") {
+          return val
+        } else if 
+          (val.Name.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+            return val
+        }
+      
+      }).map((val,key) => {
         return (
           <div className="animal" key={key}>
             <p>{val.Name} </p>
