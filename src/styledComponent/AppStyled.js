15f251e1517c77b398f95f790b6fd09e8e005245
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-28 22:39:24 -0400

    converting css to styled component

--- src/styledComponent/AppStyled.js
+++ src/styledComponent/AppStyled.js
@@ -1,2 +1,22 @@
 import styled from "styled-components";
 
+export const AppDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+export const AppInput = styled.input`
+  margin-top: 20px;
+  width: 350px;
+  height: 40px;
+  font-size: 20px;
+  padding-left: 10px;
+`;
+
+export const AppPara = styled.p`
+  margin: 10px;
+  font-size: 18px;
+`;
